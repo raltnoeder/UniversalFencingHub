fa43e9c8e5e17ff31bb7d8db680f77cc381ce161
Robert Altnoeder <r.altnoeder@gmx.net>
2019-12-16 11:45:40 +0100

    ClientException: Added param_length_error_msg helper function

--- src/client_exceptions.cpp
+++ src/client_exceptions.cpp
@@ -13,3 +13,14 @@ const char* ClientException::what() const noexcept
 {
     return message.c_str();
 }
+
+// @throws std::bad_alloc
+void ClientException::param_length_error_msg(
+    std::string& error_msg,
+    const std::string& param_key,
+    const size_t max_length
+)
+{
+    error_msg += "The value of the " + param_key + " parameter exceeds the maximum length of " +
+        std::to_string(max_length) + " bytes";
+}
--- src/client_exceptions.h
+++ src/client_exceptions.h
@@ -1,6 +1,7 @@
 #ifndef CLIENT_EXCEPTIONS_H
 #define CLIENT_EXCEPTIONS_H
 
+#include <string>
 #include <stdexcept>
 
 class ClientException : public std::exception
@@ -13,6 +14,9 @@ class ClientException : public std::exception
     virtual ClientException& operator=(const ClientException& other) = default;
     virtual ClientException& operator=(ClientException&& orig) = default;
     virtual const char* what() const noexcept override;
+
+    // @throws std::bad_alloc
+    static void param_length_error_msg(std::string& error_msg, const std::string& PARAM_KEY, const size_t MAX_LENGTH);
   private:
     std::string message;
 };
