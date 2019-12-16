1178dd749af0bf6843b005dbd24421b254cad703
Robert Altnoeder <r.altnoeder@gmx.net>
2019-12-16 11:45:48 +0100

    Added ServerInitException

--- src/server_exceptions.cpp
+++ src/server_exceptions.cpp
@@ -1,5 +1,20 @@
 #include "server_exceptions.h"
 
+// @throws std::bad_alloc
+ServerInitException::ServerInitException(const std::string& error_msg):
+    message(error_msg)
+{
+}
+
+ServerInitException::~ServerInitException() noexcept
+{
+}
+
+const char* ServerInitException::what() const noexcept
+{
+    return message.c_str();
+}
+
 PluginException::PluginException()
 {
 }
