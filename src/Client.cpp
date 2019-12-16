06e4dd5e5cb1b82e434de1b4686a9a4772fab787
Robert Altnoeder <r.altnoeder@gmx.net>
2019-12-16 11:45:36 +0100

    Client: Added constants, run method, main function

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,8 +1,34 @@
 #include "Client.h"
 #include "version.h"
 
-Client::Client()
+#include <new>
+#include <memory>
+#include <stdexcept>
+#include <iostream>
+
+const char* const Client::DEFAULT_APP_NAME = "fence-universal";
+
+const std::string Client::KEY_ACTION("action");
+const std::string Client::KEY_IPADDR("ipaddr");
+const std::string Client::KEY_PORT("port");
+const std::string Client::KEY_SECRET("secret");
+const std::string Client::KEY_NODENAME("nodename");
+
+const std::string Client::ACTION_OFF("off");
+const std::string Client::ACTION_ON("on");
+const std::string Client::ACTION_REBOOT("reboot");
+const std::string Client::ACTION_METADATA("metadata");
+const std::string Client::ACTION_STATUS("status");
+const std::string Client::ACTION_LIST("list");
+const std::string Client::ACTION_MONITOR("monitor");
+const std::string Client::ACTION_START("start");
+const std::string Client::ACTION_STOP("stop");
+
+const char Client::KEY_VALUE_SEPARATOR = '=';
+
+Client::Client(const char* const pgm_call_path_ref)
 {
+    pgm_call_path = pgm_call_path_ref;
 }
 
 Client::~Client() noexcept
@@ -17,4 +43,29 @@ const char* Client::get_version() noexcept
 uint32_t Client::get_version_code() noexcept
 {
     return ufh::VERSION_CODE;
+}
+
+// @throws std::bad_alloc
+Client::ExitCode Client::run()
+{
+    std::cout << "DEBUG: Client::run() invoked, pgm_call_path = " << pgm_call_path << std::endl;
+
+    return ExitCode::FENCING_FAILURE;
+}
+
+int main(int argc, char* argv[])
+{
+    int rc = static_cast<int> (Client::ExitCode::FENCING_FAILURE);
+    const char* pgm_call_path = argc >= 1 ? argv[0] : Client::DEFAULT_APP_NAME;
+    try
+    {
+        Client instance(pgm_call_path);
+        Client::ExitCode instance_rc = instance.run();
+        rc = static_cast<int> (instance_rc);
+    }
+    catch (std::bad_alloc&)
+    {
+        std::cerr << pgm_call_path << ": Execution failed: Out of memory" << std::endl;
+    }
+    return rc;
 }
--- src/Client.h
+++ src/Client.h
@@ -1,17 +1,52 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
 class Client
 {
   public:
-    Client();
+    enum class ExitCode : int
+    {
+        FENCING_SUCCESS = 0,
+        FENCING_FAILURE = 1
+    };
+
+    static const char* const DEFAULT_APP_NAME;
+
+    static const std::string KEY_ACTION;
+    static const std::string KEY_IPADDR;
+    static const std::string KEY_PORT;
+    static const std::string KEY_SECRET;
+    static const std::string KEY_NODENAME;
+
+    static const std::string ACTION_OFF;
+    static const std::string ACTION_ON;
+    static const std::string ACTION_REBOOT;
+    static const std::string ACTION_METADATA;
+    static const std::string ACTION_STATUS;
+    static const std::string ACTION_LIST;
+    static const std::string ACTION_MONITOR;
+    static const std::string ACTION_START;
+    static const std::string ACTION_STOP;
+
+    static const char KEY_VALUE_SEPARATOR;
+
+    const char* pgm_call_path;
+
+    Client(const char* const pgm_call_path_ref);
     virtual ~Client() noexcept;
     Client(const Client& other) = delete;
     Client(Client&& orig) = default;
     virtual Client& operator=(const Client& other) = delete;
     virtual Client& operator=(Client&& orig) = default;
 
-    virtual const uint32_t get_version_code() noexcept;
+    // @throws std::bad_alloc
+    virtual ExitCode run();
+
+    virtual uint32_t get_version_code() noexcept;
     virtual const char* get_version() noexcept;
 };
 
