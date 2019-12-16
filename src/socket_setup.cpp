510ee47a18d00f91b8bb0d2d7f8b8f3915d3740a
Robert Altnoeder <r.altnoeder@gmx.net>
2019-12-16 11:45:34 +0100

    Added socket_setup module, used for adjusting socket options

---
+++ src/socket_setup.cpp
@@ -0,0 +1,28 @@
+#include "socket_setup.h"
+
+#include <iostream>
+
+extern "C"
+{
+    #include <sys/socket.h>
+}
+
+namespace socket_setup
+{
+    void set_no_linger(const int socket_fd)
+    {
+        struct linger linger_setup;
+        linger_setup.l_onoff = 0;
+        linger_setup.l_linger = 0;
+
+        int rc = setsockopt(
+            socket_fd, SOL_SOCKET, SO_LINGER, &linger_setup,
+            static_cast<socklen_t> (sizeof (linger_setup))
+        );
+        if (rc != 0)
+        {
+            std::cout << "Warning: Clearing the SO_LINGER option for socket with socket_fd = " << socket_fd <<
+                " failed" << std::endl;
+        }
+    }
+}
---
+++ src/socket_setup.h
@@ -0,0 +1,9 @@
+#ifndef SOCKET_SETUP_H
+#define SOCKET_SETUP_H
+
+namespace socket_setup
+{
+    void set_no_linger(const int socket_fd);
+}
+
+#endif /* SOCKET_SETUP_H */
