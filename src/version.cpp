888d3bfdf9f31b42859ddc15e01cd96fb52c2113
Robert Altnoeder <r.altnoeder@gmx.net>
2023-10-04 16:05:07 +0200

    Set version 1.0.1

--- src/version.cpp
+++ src/version.cpp
@@ -2,6 +2,6 @@
 
 namespace ufh
 {
-    const uint32_t      VERSION_CODE    = 0x00010000;
-    const char* const   VERSION_STRING  = "1.0.0";
+    const uint32_t      VERSION_CODE    = 0x00010001;
+    const char* const   VERSION_STRING  = "1.0.1";
 }
